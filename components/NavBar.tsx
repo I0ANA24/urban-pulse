0b6e09b2fdb354c0705449e93601099630459b65
Denis Antoci <antoci136@gmail.com>
2026-03-03 19:18:04 +0200

    added lucide library for navbar icons

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,45 +1,33 @@
-import Image from "next/image";
 import Link from "next/link";
+import { Home, Map, MessageCircle, User } from "lucide-react";
 
 export default function NavBar() {
   return (
     <div className="h-[8vh] w-full bg-black absolute bottom-0">
       <nav className="w-full h-full flex justify-around items-center p-4">
         <Link href="/dashboard">
-          <Image
-            src="/icons/home_icon.png"
-            width={42}
-            height={37}
-            alt="home_icon"
-            className="cursor-pointer hover:scale-105 transition-all duration-300"
-          ></Image>
+          <Home
+            size={28}
+            className="text-white cursor-pointer hover:scale-105 hover:text-green-400 transition-all duration-300"
+          />
         </Link>
         <Link href="/dashboard/map">
-          <Image
-            src="/icons/map_icon.png"
-            width={51}
-            height={37}
-            alt="map_icon"
-            className="cursor-pointer hover:scale-105 transition-all duration-300"
-          ></Image>
+          <Map
+            size={28}
+            className="text-white cursor-pointer hover:scale-105 hover:text-green-400 transition-all duration-300"
+          />
         </Link>
         <Link href="/dashboard/chat">
-          <Image
-            src="/icons/message_icon.png"
-            width={51}
-            height={37}
-            alt="message_icon"
-            className="cursor-pointer hover:scale-105 transition-all duration-300"
-          ></Image>
+          <MessageCircle
+            size={28}
+            className="text-white cursor-pointer hover:scale-105 hover:text-green-400 transition-all duration-300"
+          />
         </Link>
         <Link href="/dashboard/profile">
-          <Image
-            src="/icons/profile_icon.png"
-            width={59}
-            height={43}
-            alt="profile_icon"
-            className="cursor-pointer hover:scale-105 transition-all duration-300"
-          ></Image>
+          <User
+            size={28}
+            className="text-white cursor-pointer hover:scale-105 hover:text-green-400 transition-all duration-300"
+          />
         </Link>
       </nav>
     </div>
